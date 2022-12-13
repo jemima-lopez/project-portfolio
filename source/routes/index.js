2d13f28d0561c601cc1134c23e87461f2e316e61
jemima-lopez <jlopezj11@miumg.edu.gt>
2022-12-12 23:14:18 -0600

    commit one

---
+++ source/routes/index.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+
+const router = Router()
+
+
+router.get('/', (req, res) => res.render('index', { title: 'My resume'}))
+
+router.get('/about', (req, res) => res.render('about', { title: 'About me'}))
+
+router.get('/contact', (req, res) => res.render('contact', { title: 'Contact me'}))
+
+router.get('/example', (req, res) => res.render('example', { title: 'My example'}))
+
+export default router
